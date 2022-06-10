0f7a2b291d478ca2377946708a9569f64ebb3673
Brandon <brandontrujillo1217@gmail.com>
2022-06-10 00:06:38 -0500

    Rutas de producto funcionando, al igual que la actualización del total de productos después de agregar cierta cantidad al carrito

--- Cliente/src/views/logica/producto_cliente.js
+++ Cliente/src/views/logica/producto_cliente.js
@@ -1,5 +1,5 @@
 
-const URL = "http:/localhost:4000/";
+const URLHost = "http:/localhost:4000/";
 var params = window.location.search.substring(1);
 var idUsuario = localStorage.getItem('idUsuario');
 
@@ -18,7 +18,7 @@ function cargarProducto(){
         mostrarProducto(data);
     }
     };
-    xhttp.open("GET", URL+"productos/"+extraerParams(), true);
+    xhttp.open("GET", URLHost+"productos/"+extraerParams(), true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 }
@@ -31,31 +31,50 @@ function mostrarProducto(data){
 
 function añadirCarrito(){
     let cantidad = document.getElementById("txt_cantidad").value;
-    if(cantidad){
-        let agregarCarrito = {
-            "idUsuario": idUsuario,
-            "idProducto": extraerParams(),
-            "cantidad": cantidad,
-            "total": precioProducto * cantidad
-        }
-
-        var request = new XMLHttpRequest();
-        request.open('POST', URL+'productos/agregarCarrito', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.onload = function(){
-            
-            if(this.response == 1){
-                alert("Producto agregado exitosamente");
-                window.open("Aquí va la ruta para abrir el carrito de compras");
-            }else{
-                alert("No fue posible realizar el registro");
+    try {
+        if(cantidad){
+            let agregarCarrito = {
+                "idUsuario": idUsuario,
+                "idProducto": extraerParams(),
+                "cantidad": cantidad,
+                "total": precioProducto * cantidad
             }
-           
+    
+            var request = new XMLHttpRequest();
+            request.open('POST', URLHost+'agregarCarrito', true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.onload = function(){
+                
+                if(this.response == 1){
+                    let restarCantidad = {
+                        "cantidad": cantidad,
+                        "idProducto": extraerParams()
+                    }
+                    var request2 = new XMLHttpRequest();
+                    request2.open('PUT', URLHost+'restarProductos', true);
+                    request2.setRequestHeader('Content-Type', 'application/json');
+                    request2.onload = function(){
+                        if(this.response == 1){
+                            alert("Producto agregado exitosamente");
+                            window.open('../vista_consumidor/carritoCompras.html?idUsuario='+idUsuario, '_self');
+                        }else{
+                            alert("El producto se agregó al carrito pero no se actualizó el total de productos restantes");
+                        }
+                    }
+                    request2.send(JSON.stringify(restarCantidad));
+                }else{
+                    alert("No fue posible realizar el registro del producto al carrito");
+                }
+               
+            }
+            request.send(JSON.stringify(agregarCarrito));
+        }else{
+            document.getElementById("txt_cantidad").style.borderColor = "red";
         }
-        request.send(JSON.stringify(agregarCarrito));
-    }else{
-        document.getElementById("txt_cantidad").style.borderColor = "red";
+    } catch (error) {
+        alert("Ocurrió un error, intente más tarde o comuniquese con los profesionales")
     }
+    
 }
 
 
--- Servicio/src/index.js
+++ Servicio/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', process.env.port || 4000);
 
 app.use(express.json());
-app.use('/productos', require('./rutas/productos'))
+app.use(require('./rutas/productos'))
 app.use('/usuario', require('./rutas/usuario'))
 app.use('/categorias',require('./rutas/categorias'))
 app.use('/reportes',require('./rutas/reportes'))
