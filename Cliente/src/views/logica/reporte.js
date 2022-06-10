0aa847af79698e11ab7f7ef3e8df31b445f76e92
Mario Dorantes <mario13.MEDH@gmail.com>
2022-06-10 17:21:20 -0500

    muestra la imagen en el reporte, me falta guardarla

--- Cliente/src/views/logica/reporte.js
+++ Cliente/src/views/logica/reporte.js
@@ -1,5 +1,5 @@
 
-const URL = "http:/localhost:4000/";
+//const URL = "http:/localhost:4000/";
 var params = window.location.search.substring(1);
 var idUsuario = localStorage.getItem('idUsuario');
 
@@ -8,13 +8,20 @@ function registrarReporte() {
 
     let txtAsunto = formularioRegistrarReporte.txtAsunto.value;
     let txtComentarios =formularioRegistrarReporte.txtComentarios.value;
-    //espacio para fotografia
-
+    let imagenReporteVista = formularioRegistrarReporte.txtImagenReporte.files[0];
+    /*
     let reporte = {
         comentarios:txtComentarios,
         tipo:txtAsunto,
         idUsuario:idUsuario
-    }
+    }*/
+
+    var reporte = new FormData();
+
+    reporte.append("comentarios", txtComentarios);
+    producto.append("tipo", txtAsunto);
+    producto.append("idUsuario", idUsuario);
+    producto.append("imagen", imagenReporteVista);
 
     var request = new XMLHttpRequest();
 
@@ -40,4 +47,36 @@ function registrarReporte() {
     }
 
     request.send(JSON.stringify(reporte));
+}
+
+function visualizarImagenReporte() {
+
+    const txtImagenReporte = document.getElementById("txtImagenReporte");
+    const imagenReporteVista = document.getElementById("imagenReporteVista");
+    
+    imagenReporteVista.setAttribute("class","img-fluid");
+
+    if (txtImagenReporte.files != 0) {
+        
+        const tipoImagen = [
+            "image/apng",
+            "image/bmp",
+            "image/gif",
+            "image/jpeg",
+            "image/pjpeg",
+            "image/png",
+            "image/svg+xml",
+            "image/tiff",
+            "image/webp",
+            "image/x-icon"
+        ];
+
+        for(const imagen of txtImagenReporte.files) {
+
+            if(tipoImagen.includes(imagen.type)){
+                imagenReporteVista.src = URL.createObjectURL(imagen);
+            }
+        }
+    }
+
 }
